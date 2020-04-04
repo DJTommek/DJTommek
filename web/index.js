1891561e37143a96aacf1f529a50f6f5a5bc967a
DJTommek <tomas@palider.cz>
2020-04-05 00:50:38 +0200

    Reformatted by IDEA

--- web/index.js
+++ web/index.js
@@ -2,48 +2,48 @@
  * On page load
  */
 $(function () {
-    calculateDetailedAge();
-    // enable tooltips for all elements with title (use custom template because little arrow looks bad in dark theme)
-  	$('[title]').tooltip({template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>'});
+	calculateDetailedAge();
+	// enable tooltips for all elements with title (use custom template because little arrow looks bad in dark theme)
+	$('[title]').tooltip({template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>'});
 
-    /**
-     * Before opening tooltip on birth date
-     */
-    $('#birth-date').on('show.bs.tooltip', function () {
-        calculateDetailedAge();
-    }).tooltip();
+	/**
+	 * Before opening tooltip on birth date
+	 */
+	$('#birth-date').on('show.bs.tooltip', function () {
+		calculateDetailedAge();
+	}).tooltip();
 
-    /**
-     * Toggle dark theme icon
-     */
-    $('#toggle-theme').on('click', function (e) {
-        e.preventDefault();
-        const bodyEl = $('body');
-        bodyEl.toggleClass('dark');
-        try {
-            localStorage.setItem("dark-theme", bodyEl.hasClass('dark').toString());
-        } catch (error) {
-            // Local storage is not working (disabled, unsupported, ...)
-        }
-    });
+	/**
+	 * Toggle dark theme icon
+	 */
+	$('#toggle-theme').on('click', function (e) {
+		e.preventDefault();
+		const bodyEl = $('body');
+		bodyEl.toggleClass('dark');
+		try {
+			localStorage.setItem("dark-theme", bodyEl.hasClass('dark').toString());
+		} catch (error) {
+			// Local storage is not working (disabled, unsupported, ...)
+		}
+	});
 });
 
 /**
  * Just calculate and update age, obviously...
  */
 function calculateDetailedAge() {
-    const baseElement = $('#birth-date');
-    const birthDate = new Date(baseElement.text().trim());
-    const diff = msToHuman((new Date()) - birthDate);
-    baseElement.attr('data-original-title', diff + ' ago').tooltip();
-    
-    const age = Math.floor((new Date() - birthDate) / (365 * 24 * 60 * 60 * 1000));
-    $('#age').text('(' + age + ' years)');  
+	const baseElement = $('#birth-date');
+	const birthDate = new Date(baseElement.text().trim());
+	const diff = msToHuman((new Date()) - birthDate);
+	baseElement.attr('data-original-title', diff + ' ago').tooltip();
+
+	const age = Math.floor((new Date() - birthDate) / (365 * 24 * 60 * 60 * 1000));
+	$('#age').text('(' + age + ' years)');
 }
 
 /**
- * Convert miliseconds to short human readable format 
- * 
+ * Convert miliseconds to short human readable format
+ *
  * @param {number} miliseconds
  * @returns {String}
  */
@@ -60,18 +60,18 @@ function msToHuman(miliseconds) {
 	const minutes = Math.floor((miliseconds / (1000 * 60)) % 60);
 	const hours = Math.floor((miliseconds / (1000 * 60 * 60)) % 24);
 	const days = Math.floor((miliseconds / (1000 * 60 * 60 * 24)) % 30);
-    const months = Math.floor((miliseconds / (1000 * 60 * 60 * 24 * 30.4366)) % 12);
-    const years = Math.floor((miliseconds / (1000 * 60 * 60 * 24 * 365.24)));
+	const months = Math.floor((miliseconds / (1000 * 60 * 60 * 24 * 30.4366)) % 12);
+	const years = Math.floor((miliseconds / (1000 * 60 * 60 * 24 * 365.24)));
 
-    let result = '';
-    result += (years > 0 ? ' ' + years + 'y' : '');
-    result += (months > 0 ? ' ' + months + 'mo' : '');
-    result += (days > 0 ? ' ' + days + 'd' : '');
-    result += (hours > 0 ? ' ' + hours + 'h' : '');
-    result += (minutes > 0 ? ' ' + minutes + 'mi' : '');
-    result += (seconds > 0 ? ' ' + seconds + 's' : '');
-    result += (milliseconds > 0 ? ' ' + milliseconds + 'ms' : '');
-    return result.trim();
+	let result = '';
+	result += (years > 0 ? ' ' + years + 'y' : '');
+	result += (months > 0 ? ' ' + months + 'mo' : '');
+	result += (days > 0 ? ' ' + days + 'd' : '');
+	result += (hours > 0 ? ' ' + hours + 'h' : '');
+	result += (minutes > 0 ? ' ' + minutes + 'mi' : '');
+	result += (seconds > 0 ? ' ' + seconds + 's' : '');
+	result += (milliseconds > 0 ? ' ' + milliseconds + 'ms' : '');
+	return result.trim();
 }
 
 /**
@@ -92,8 +92,7 @@ Array.prototype.inArray = function (element) {
  * @param {String} [dir] (left, both, right = default)
  * @returns {String}
  */
-String.prototype.pad = String.prototype.pad || function (length, string, type)
-{
+String.prototype.pad = String.prototype.pad || function (length, string, type) {
 	let str = this;
 
 	// validation of length
